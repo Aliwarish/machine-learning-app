5c98ea646b9d18875f6712142b5d7e0fe04dfae0
Aliwarish <112713906+Aliwarish@users.noreply.github.com>
2023-06-02 23:49:41 +0530

    Update aliapp.py

--- aliapp.py
+++ aliapp.py
@@ -2,7 +2,7 @@ import streamlit as st
 page_bg_img = f"""
 <style>
 [data-testid="stAppViewContainer"]{{
-background-image: url("https://wallpaperaccess.com/full/550746.jpg");
+background-image: url("https://wallpaperaccess.com/full/166010.jpg");
 backgroung-size: cover;
 }}
 
