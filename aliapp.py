c395dfb66849301402b4ae4fff84bbb01039d57a
Aliwarish <112713906+Aliwarish@users.noreply.github.com>
2023-06-05 22:12:17 +0530

    Update aliapp.py

--- aliapp.py
+++ aliapp.py
@@ -2,7 +2,7 @@ import streamlit as st
 page_image_bg = f"""
 <style>
 [data-testid="stAppViewContainer"]{{
-background-image: url("https://global.discourse-cdn.com/business7/uploads/streamlit/original/3X/5/3/53d052dd9cb413893695f81eb3e524a955a96970.jpeg");
+background-image: url("https://cdn.pixabay.com/photo/2019/04/24/11/27/flowers-4151900_960_720.jpg");
 background-size: cover;
 background-repeat: no repeat;
 }}
